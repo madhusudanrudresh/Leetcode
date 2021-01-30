6da1c3c0c87eb319494c912f1c55ad64773ebf13
Madhusudan <madhusudanrudresh@gmail.com>
2021-01-30 17:15:01 -0600

    Winning Candidate

---
+++ Database/winning_candidate.sql
@@ -0,0 +1,47 @@
+/*
+Table: Candidate
+
++-----+---------+
+| id  | Name    |
++-----+---------+
+| 1   | A       |
+| 2   | B       |
+| 3   | C       |
+| 4   | D       |
+| 5   | E       |
++-----+---------+  
+Table: Vote
+
++-----+--------------+
+| id  | CandidateId  |
++-----+--------------+
+| 1   |     2        |
+| 2   |     4        |
+| 3   |     3        |
+| 4   |     2        |
+| 5   |     5        |
++-----+--------------+
+id is the auto-increment primary key,
+CandidateId is the id appeared in Candidate table.
+Write a sql to find the name of the winning candidate, the above example will return the winner B.
+
++------+
+| Name |
++------+
+| B    |
++------+
+Notes:
+
+You may assume there is no tie, in other words there will be only one winning candidate.
+*/
+
+# Write your MySQL query statement below
+
+with cte as (
+    select candidateid, count(id) from vote
+    group by candidateid
+    order by count(id) desc
+    limit 1
+)
+select name from candidate
+where id in (select candidateid from cte)
