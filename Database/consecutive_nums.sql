bbe22327fc3c1578a9682169de456758bd4a5cbf
Madhusudan <madhusudanrudresh@gmail.com>
2021-01-30 11:54:56 -0600

    SQL Practice

---
+++ Database/consecutive_nums.sql
@@ -0,0 +1,52 @@
+"""
+
+
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+id is the primary key for this table.
+ 
+
+Write an SQL query to find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The query result format is in the following example:
+
+ 
+
+Logs table:
++----+-----+
+| Id | Num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+
+Result table:
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+1 is the only number that appears consecutively for at least three times.
+
+"""
+
+# Write your MySQL query statement below
+
+with cte as (
+    select id, num, lead(num) over (order by id asc) ld1, lead(num, 2) over (order by id asc) ld2 from logs
+) 
+select distinct num as ConsecutiveNums from cte
+where num = ld1 and ld1 = ld2
