dfa6a4b44ebf8707c96bc01548a80673ef41e38b
Madhusudan <madhusudanrudresh@gmail.com>
2021-01-31 23:12:30 -0600

    Active Businesses

---
+++ Database/active_businesses.sql
@@ -0,0 +1,63 @@
+/*
+
+Table: Events
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| business_id   | int     |
+| event_type    | varchar |
+| occurences    | int     | 
++---------------+---------+
+(business_id, event_type) is the primary key of this table.
+Each row in the table logs the info that an event of some type occured at some business for a number of times.
+ 
+
+Write an SQL query to find all active businesses.
+
+An active business is a business that has more than one event type with occurences greater than the average occurences of that event type among all businesses.
+
+The query result format is in the following example:
+
+Events table:
++-------------+------------+------------+
+| business_id | event_type | occurences |
++-------------+------------+------------+
+| 1           | reviews    | 7          |
+| 3           | reviews    | 3          |
+| 1           | ads        | 11         |
+| 2           | ads        | 7          |
+| 3           | ads        | 6          |
+| 1           | page views | 3          |
+| 2           | page views | 12         |
++-------------+------------+------------+
+
+Result table:
++-------------+
+| business_id |
++-------------+
+| 1           |
++-------------+ 
+Average for 'reviews', 'ads' and 'page views' are (7+3)/2=5, (11+7+6)/3=8, (3+12)/2=7.5 respectively.
+Business with id 1 has 7 'reviews' events (more than 5) and 11 'ads' events (more than 8) so it is an active business.
+
+*/
+
+# Write your MySQL query statement below
+
+with event_avg_cte as (
+    select event_type, avg(occurences) avg_occ
+    from events
+    group by event_type
+), 
+compare_cte as (
+    select e.business_id, e.event_type, 
+    case when e.occurences > eac.avg_occ then 1 else 0 end as compare_occ
+    from events e
+    inner join event_avg_cte eac on e.event_type = eac.event_type
+)
+select business_id
+from compare_cte
+group by business_id
+having sum(compare_occ) > 1
+
