12005694c267ac3f928116a969c838d0e8812799
Madhusudan <madhusudanrudresh@gmail.com>
2021-02-02 23:39:03 -0600

    Customers who bought A, B and not C

---
+++ Database/customers_who_bought_ab_not_c.sql
@@ -0,0 +1,84 @@
+/*
+
+Table: Customers
+
++---------------------+---------+
+| Column Name         | Type    |
++---------------------+---------+
+| customer_id         | int     |
+| customer_name       | varchar |
++---------------------+---------+
+customer_id is the primary key for this table.
+customer_name is the name of the customer.
+ 
+
+Table: Orders
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| order_id      | int     |
+| customer_id   | int     |
+| product_name  | varchar |
++---------------+---------+
+order_id is the primary key for this table.
+customer_id is the id of the customer who bought the product "product_name".
+ 
+
+Write an SQL query to report the customer_id and customer_name of customers who bought products "A", "B" but did not buy the product "C" since we want to recommend them buy this product.
+
+Return the result table ordered by customer_id.
+
+The query result format is in the following example.
+
+ 
+
+Customers table:
++-------------+---------------+
+| customer_id | customer_name |
++-------------+---------------+
+| 1           | Daniel        |
+| 2           | Diana         |
+| 3           | Elizabeth     |
+| 4           | Jhon          |
++-------------+---------------+
+
+Orders table:
++------------+--------------+---------------+
+| order_id   | customer_id  | product_name  |
++------------+--------------+---------------+
+| 10         |     1        |     A         |
+| 20         |     1        |     B         |
+| 30         |     1        |     D         |
+| 40         |     1        |     C         |
+| 50         |     2        |     A         |
+| 60         |     3        |     A         |
+| 70         |     3        |     B         |
+| 80         |     3        |     D         |
+| 90         |     4        |     C         |
++------------+--------------+---------------+
+
+Result table:
++-------------+---------------+
+| customer_id | customer_name |
++-------------+---------------+
+| 3           | Elizabeth     |
++-------------+---------------+
+Only the customer_id with id 3 bought the product A and B but not the product C.
+
+*/
+
+# Write your MySQL query statement below
+
+with cte as (
+    select customer_id, 
+        sum(case when product_name = 'A' then 1 else 0 end) product_A,
+        sum(case when product_name = 'B' then 1 else 0 end) product_B,
+        sum(case when product_name = 'C' then 1 else 0 end) product_C
+    from orders
+    group by customer_id    
+)
+select cte.customer_id, c.customer_name 
+from cte
+inner join customers c on cte.customer_id = c.customer_id
+where product_A = 1 and product_B = 1 and product_C = 0
